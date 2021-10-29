b913e0b1bfe5006d820b777625f1f5d9031a1ce1
Sean Miningah <miningahsean@gmail.com>
2021-10-29 04:29:12 +0300

    user app replacing authentication(handled by firebase)

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+
+from .models import RetailerInfo, Shop
+from .serializer import RetailerInfoSerializer, ShopSerializer
+
+# Create your views here.
+
+
+class RetailerInfoViewSet(viewsets.ModelViewSet):
+    queryset = RetailerInfo.objects.all().order_by('-registration_date')
+    serializer_class = RetailerInfoSerializer
+
+
+class ShopViewSet(viewsets.ModelViewSet):
+    queryset = Shop.objects.all().order_by('registration_date')
+    serializer_class = ShopSerializer
