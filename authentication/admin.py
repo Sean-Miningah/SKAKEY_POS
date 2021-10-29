88b0665a027683c0f5a7f24752126fba4a497392
Sean Miningah <miningahsean@gmail.com>
2021-10-29 03:42:26 +0300

    retailer and shop models added to the administrator

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from .models import RetailerInfo, Shop
 
 # Register your models here.
-admin.site.register(Hero)
+admin.site.register(RetailerInfo)
+admin.site.register(Shop)
