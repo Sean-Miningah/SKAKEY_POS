4736ef952ca6fcd5a5f6bf3db40e82fc5d458540
Sean Miningah <miningahsean@gmail.com>
2021-12-08 20:55:34 +0300

    migrations and url update

--- shops/urls.py
+++ shops/urls.py
@@ -3,15 +3,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-<<<<<<< HEAD
-router.register(r'create-shop', views.ShopViewSet)
-router.register(r'products', views.ShopProductViewSet)
-=======
 router.register(r'create-shops', views.ShopViewSet)
 router.register(r'add_category', views.ProductCategoryViewSet)
 router.register(r'cart', views.ShopCartViewSet)
 router.register(r'add-products', views.ShopProductViewSet)
->>>>>>> create-shop
 
 urlpatterns = [
     path('', include(router.urls)),
